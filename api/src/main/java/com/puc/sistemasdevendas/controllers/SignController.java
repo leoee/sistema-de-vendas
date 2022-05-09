8e41b53525a7babc1d12e6d78f1b4d78b881b53f
leoee <leonardoe1997@gmail.com>
2022-05-09 20:39:01 -0300

    Map authentication response

--- api/src/main/java/com/puc/sistemasdevendas/controllers/SignController.java
+++ api/src/main/java/com/puc/sistemasdevendas/controllers/SignController.java
@@ -1,5 +1,6 @@
 package com.puc.sistemasdevendas.controllers;
 
+import com.puc.sistemasdevendas.model.entities.AuthenticationResponse;
 import com.puc.sistemasdevendas.model.entities.UserLogin;
 import com.puc.sistemasdevendas.model.services.SignInOutService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,7 @@ public class SignController {
     private SignInOutService signService;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    ResponseEntity<?> authorizeUser(@RequestBody @Valid UserLogin userPayload) throws UnsupportedEncodingException {
-        return ResponseEntity.ok(this.signService.authorizeUser(userPayload));
+    ResponseEntity<AuthenticationResponse> authorizeUser(@RequestBody @Valid UserLogin userPayload) throws UnsupportedEncodingException {
+        return ResponseEntity.ok(new AuthenticationResponse(this.signService.authorizeUser(userPayload)));
     }
 }
