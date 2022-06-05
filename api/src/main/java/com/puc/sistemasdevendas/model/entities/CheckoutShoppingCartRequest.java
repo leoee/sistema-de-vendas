04cdacae586794a6c2625a3262ef80dbfab60abb
leoee <leonardoe1997@gmail.com>
2022-06-05 09:43:27 -0300

    Add annotation to require payment method

--- api/src/main/java/com/puc/sistemasdevendas/model/entities/CheckoutShoppingCartRequest.java
+++ api/src/main/java/com/puc/sistemasdevendas/model/entities/CheckoutShoppingCartRequest.java
@@ -2,8 +2,11 @@ package com.puc.sistemasdevendas.model.entities;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CheckoutShoppingCartRequest {
     private String notesForDelivery;
+    @NotNull(message = "A payment method is required.")
     private PaymentMethods paymentMethod;
 }
