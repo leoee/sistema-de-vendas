6327854fe479278d17c6776cb1e6d130570fd2ec
leoee <leonardoe1997@gmail.com>
2022-06-12 19:50:46 -0300

    Refactor my-orders page to remove unused variables

--- web/web-view/src/app/layout/my-orders/my-orders.component.ts
+++ web/web-view/src/app/layout/my-orders/my-orders.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { NotificationService } from 'src/app/core/services/notification.service';
 import { Item } from 'src/app/data/dtos/item.model';
-import { ItemService } from 'src/app/data/services/item.service';
 import { OrderService } from 'src/app/data/services/order.service';
 
 @Component({
@@ -14,19 +11,8 @@ import { OrderService } from 'src/app/data/services/order.service';
   styleUrls: ['./my-orders.component.css']
 })
 export class MyOrdersComponent implements OnInit {
-  public items$!: Observable<any>
-  public item$!: Observable<any>
-  public shoppingCart$!: Observable<any>
   public orders$!: Observable<any>
-  public currentItemToDelete: Item;
-  public currentItemToEdit: any;
   private unsub$ = new Subject<void>();
-  public name: string;
-  public minPrice: string;
-  public maxPrice: string;
-  public stockQuantity: string;
-  public form!: FormGroup
-  public checkoutForm!: FormGroup
 
   public mapOrderStatus = new Map<string, string>([
     ["WAITING_PAYMENT", "AGUARDANDO PAGAMENTO"],
@@ -50,25 +36,11 @@ export class MyOrdersComponent implements OnInit {
   public error$ = new Subject<boolean>();
 
   constructor(
-    private readonly formBuilder: FormBuilder,
-    private readonly itemService: ItemService,
     private readonly modal: NgbModal,
-    private readonly notificationService: NotificationService,
     private readonly orderService: OrderService) { }
 
   public ngOnInit(): void {
     this.loadOrders();
-    this.initializeForms();
-  }
-
-  private initializeForms (): void {
-    this.form = this.formBuilder.group({
-      amount: ['', [Validators.required]]
-    });
-    this.checkoutForm = this.formBuilder.group({
-      notesForDelivery: [''],
-      paymentMethod: ['', [Validators.required]]
-    });
   }
 
   public ngOnDestroy (): void {
@@ -77,105 +49,17 @@ export class MyOrdersComponent implements OnInit {
   }
 
   private loadOrders(): void {
-    let that = this;
     this.orders$ = this.orderService
     .loadOrders()
     .pipe(map(orders => orders));
   }
 
-  public openViewItemModal (content: any, item: any): void {
-    this.item$ = this.itemService
-    .loadItemById(item.itemId)
-    .pipe(map(item => item));
+  public openViewModal (content: any): void {
     this.modal.open(content, { centered: true })
       .result.then(
         () => {
         },
         () => this.modal.dismissAll()
       )
-  }
-
-  public openDeleteShoppingCarttemModal (content: any, item: any): void {
-    this.currentItemToDelete = item;
-    this.modal.open(content, { centered: true })
-      .result.then(
-        () => {
-          this.deleteItemOnShoppingCart(item)
-        },
-        () => this.modal.dismissAll()
-      )
-  }
-
-  public openEditShoppingCartItemAmount (content: any, item: any): void {
-    this.currentItemToEdit = item;
-    this.modal.open(content, { centered: true })
-      .result.then(
-        () => {
-        },
-        () => this.modal.dismissAll()
-      )
-  }
-
-  public openCheckoutShoppingCart (content: any): void {
-    this.modal.open(content, { centered: true })
-      .result.then(
-        () => {
-        },
-        () => this.modal.dismissAll()
-      )
-  }
-
-  public updateItemAmont(item: any): void {
-    /*this.shoppingCartService.updateShoppingCartItem(this.currentItemToEdit.itemId, item)
-    .pipe(takeUntil(this.unsub$))
-    .subscribe(
-      () => {
-        this.notificationService.success('Atenção!', this.currentItemToEdit.name + ' foi atualizado do seu carrinho.')
-        this.loadShoppingCart()
-      },
-      (error: HttpErrorResponse) => {
-        if (error.status == 403) {
-          this.notificationService.error('Ops!', 'Você não pode executar essa ação.')
-        } else {
-          this.notificationService.error('Ops!', 'Você está tentando uma atualização inválida.')
-        }
-      }
-    )*/
-  }
-
-  private deleteItemOnShoppingCart(item: any): void {
-    /*this.shoppingCartService.deleteShoppingCartItem(item.itemId)
-    .pipe(takeUntil(this.unsub$))
-    .subscribe(
-      () => {
-        this.notificationService.success('Atenção!', item.name + ' foi removido do seu carrinho.')
-        this.loadShoppingCart()
-      },
-      (error: HttpErrorResponse) => {
-        if (error.status == 403) {
-          this.notificationService.error('Ops!', 'Você não pode executar essa ação.')
-        } else {
-          this.notificationService.error('Ops!', 'Algo deu errado. Tente novamente.')
-        }
-      }
-    )*/
-  }
-
-  public checkoutShoppingCart(requestPayload: any): void {
-    /*this.shoppingCartService.checkoutShoppingCart(requestPayload)
-    .pipe(takeUntil(this.unsub$))
-    .subscribe(
-      () => {
-        this.notificationService.success('Que legal!', 'Seu carrinho foi finalizado com sucesso. Agora basta ficar de olho no seus pedidos!!')
-        this.loadShoppingCart()
-      },
-      (error: HttpErrorResponse) => {
-        if (error.status == 403) {
-          this.notificationService.error('Ops!', 'Você não pode executar essa ação.')
-        } else {
-          this.notificationService.error('Ops!', 'Selecione um método de pagamento e certifique que seu carrinho não está vazio.')
-        }
-      }
-    )*/
   }
 }
